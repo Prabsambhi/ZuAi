7b9460c61c2c578d7b82dc519f014810f6a5fccb
Prabsambhi <prabsambhi@gmail.com>
2024-08-25 22:15:21 +0530

    again

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build-output', // Specify your custom build output directory
+    outDir: "build-output",
+  },
+  server: {
+    proxy: {
+      "/api": "http://localhost:3000", 
+    },
   },
 });
